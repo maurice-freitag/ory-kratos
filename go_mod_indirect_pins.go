1551ac19f1fc2ead4202e7d7cc0a6055e721f2e5
hackerman <3372410+aeneasr@users.noreply.github.com>
2020-06-24 15:01:24 +0200

    fix: write migration tests (#499)
    Closes #481

--- go_mod_indirect_pins.go
+++ go_mod_indirect_pins.go
@@ -1,16 +1,28 @@
-//go:build go_mod_indirect_pins
-// +build go_mod_indirect_pins
+// +build tools
 
 package main
 
 import (
-	_ "github.com/go-bindata/go-bindata/go-bindata"
 	_ "github.com/go-swagger/go-swagger/cmd/swagger"
-	_ "github.com/golang/mock/mockgen"
-	_ "github.com/mikefarah/yq/v4"
-	_ "golang.org/x/tools/cmd/goimports"
-	_ "golang.org/x/tools/cmd/stringer"
-	_ "gopkg.in/DataDog/dd-trace-go.v1/ddtrace"
+	_ "github.com/mattn/goveralls"
+	_ "github.com/sqs/goreturns"
+	_ "golang.org/x/tools/cmd/cover"
+
+	_ "github.com/sqs/goreturns"
+
+	_ "github.com/gobuffalo/fizz"
 
 	_ "github.com/ory/go-acc"
+	_ "github.com/ory/x/tools/listx"
+
+	_ "github.com/gobuffalo/packr/v2"
+	_ "github.com/jteeuwen/go-bindata"
+
+	_ "github.com/mikefarah/yq"
+
+	_ "github.com/davidrjonas/semver-cli"
+
+	_ "github.com/hashicorp/consul/api"
+
+	_ "github.com/ory/cli"
 )
--- persistence/sql/migratest/migration_test.go
+++ persistence/sql/migratest/migration_test.go
@@ -2,170 +2,200 @@ package migratest
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
+	"runtime/debug"
+	"strconv"
 	"testing"
 
-	"github.com/gobuffalo/pop/v6"
+	"github.com/gobuffalo/packr/v2/plog"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
 
-	"github.com/ory/x/configx"
+	"github.com/ory/x/sqlcon"
 
-	"github.com/ory/hydra/persistence/sql"
-
+	"github.com/ory/viper"
+	"github.com/ory/x/logrusx"
 	"github.com/ory/x/popx"
-
 	"github.com/ory/x/sqlcon/dockertest"
 
-	"github.com/ory/hydra/driver/config"
-
+	"github.com/gobuffalo/pop/v5"
 	"github.com/stretchr/testify/require"
 
-	"github.com/ory/hydra/client"
-	"github.com/ory/hydra/consent"
-	"github.com/ory/hydra/driver"
-	"github.com/ory/hydra/jwk"
-	"github.com/ory/hydra/oauth2"
-	"github.com/ory/hydra/x"
+	gobuffalologger "github.com/gobuffalo/logger"
+
+	"github.com/ory/kratos/driver"
+	"github.com/ory/kratos/driver/configuration"
+	"github.com/ory/kratos/internal/testhelpers"
+	"github.com/ory/kratos/selfservice/flow/login"
+	"github.com/ory/kratos/selfservice/flow/recovery"
+	"github.com/ory/kratos/selfservice/flow/registration"
+	"github.com/ory/kratos/selfservice/flow/settings"
+	"github.com/ory/kratos/x"
 )
 
 func TestMigrations(t *testing.T) {
-	if testing.Short() {
-		t.SkipNow()
-		return
-	}
-
-	connections := make(map[string]*pop.Connection, 3)
-	dockertest.Parallel([]func(){
-		func() {
-			connections["postgres"] = dockertest.ConnectToTestPostgreSQLPop(t)
-		},
-		func() {
-			connections["mysql"] = dockertest.ConnectToTestMySQLPop(t)
-		},
-		func() {
-			connections["cockroach"] = dockertest.ConnectToTestCockroachDBPop(t)
-		},
+	sqlite, err := pop.NewConnection(&pop.ConnectionDetails{
+		URL: "sqlite3://" + filepath.Join(os.TempDir(), x.NewUUID().String()) + ".sql?mode=memory&_fk=true",
 	})
+	require.NoError(t, err)
+	require.NoError(t, sqlite.Open())
 
-	for db, c := range connections {
-		t.Run(fmt.Sprintf("database=%s", db), func(t *testing.T) {
-			x.CleanSQLPop(t, c)
-			url := c.URL()
+	connections := map[string]*pop.Connection{
+		"sqlite": sqlite,
+	}
+	l := logrusx.New("", "", logrusx.ForceLevel(logrus.TraceLevel))
+	plog.Logger = gobuffalologger.Logrus{FieldLogger: l.Entry}
 
-			// workaround for https://github.com/gobuffalo/pop/issues/538
-			if db == "mysql" {
-				url = "mysql://" + url
-			}
-
-			d := driver.New(
-				context.Background(),
-				driver.WithOptions(configx.WithValue(config.KeyDSN, url)),
-				driver.DisablePreloading(),
-				driver.DisableValidation(),
-			)
-
-			tm := popx.NewTestMigrator(t, c, os.DirFS("../migrations"), os.DirFS("./testdata"), d.Logger())
-			require.NoError(t, tm.Up(context.Background()))
-
-			var lastClient *client.Client
-			for i := 1; i <= 14; i++ {
-				// skip cockroach assertions until migration 13
-				if db == "cockroach" && i < 13 {
-					continue
-				}
-				t.Run(fmt.Sprintf("case=client migration %d", i), func(t *testing.T) {
-					expected := expectedClient(i)
-					actual := &client.Client{}
-					require.NoError(t, c.Where("id = ?", expected.OutfacingID).First(actual))
-					assertEqualClients(t, expected, actual)
-					lastClient = actual
-				})
-			}
-
-			for i := 1; i <= 4; i++ {
-				// skip cockroach assertions until migration 4
-				if db == "cockroach" && i < 4 {
-					continue
-				}
-				t.Run(fmt.Sprintf("case=jwk migration %d", i), func(t *testing.T) {
-					expected := expectedJWK(i)
-					actual := &jwk.SQLData{}
-					require.NoError(t, c.Where("pk = ?", expected.ID).First(actual))
-					assertEqualJWKs(t, expected, actual)
-				})
-			}
-
-			for i := 1; i <= 14; i++ {
-				if db == "cockroach" && i < 12 {
-					continue
-				}
-				t.Run(fmt.Sprintf("case=consent migration %d", i), func(t *testing.T) {
-					ecr, elr, els, ehcr, ehlr, efols, elor := expectedConsent(i)
-
-					acr, err := d.ConsentManager().GetConsentRequest(context.Background(), ecr.ID)
-					require.NoError(t, err)
-					assertEqualConsentRequests(t, ecr, acr)
-
-					alr, err := d.ConsentManager().GetLoginRequest(context.Background(), elr.ID)
-					require.NoError(t, err)
-					assertEqualLoginRequests(t, elr, alr)
-
-					als := &consent.LoginSession{}
-					require.NoError(t, c.Find(als, els.ID))
-					assertEqualLoginSessions(t, els, als)
-
-					ahcr := &consent.HandledConsentRequest{}
-					require.NoError(t, c.Q().Where("challenge = ?", ehcr.ID).First(ahcr))
-					require.NoError(t, ehcr.AfterFind(c))
-					assertEqualHandledConsentRequests(t, ehcr, ahcr)
-
-					ahlr := &consent.HandledLoginRequest{}
-					require.NoError(t, c.Q().Where("challenge = ?", ehlr.ID).First(ahlr))
-					assertEqualHandledLoginRequests(t, ehlr, ahlr)
-
-					if efols != nil {
-						afols, err := d.ConsentManager().GetForcedObfuscatedLoginSession(context.Background(), lastClient.OutfacingID, efols.SubjectObfuscated)
-						require.NoError(t, err)
-						assertEqualForcedObfucscatedLoginSessions(t, efols, afols)
-					}
-
-					if elor != nil {
-						alor := &consent.LogoutRequest{}
-						require.NoError(t, d.Persister().Connection(context.Background()).RawQuery("select * from hydra_oauth2_logout_request where challenge = ?", elor.ID).First(alor))
-						alor.Client = nil
-						assertEqualLogoutRequests(t, elor, alor)
-					}
-				})
-			}
-
-			// TODO https://github.com/ory/hydra/issues/1815
-			// this is very stupid and should be replaced as soon the manager uses pop
-			// necessary because the manager does not provide any way to access the data
-			for i := 1; i <= 11; i++ {
-				if db == "cockroach" && i < 9 {
-					continue
-				}
-
-				tables := []string{"hydra_oauth2_access", "hydra_oauth2_refresh", "hydra_oauth2_code", "hydra_oauth2_oidc"}
-				if i >= 3 {
-					tables = append(tables, "hydra_oauth2_pkce")
-				}
-				ed, ebjti := expectedOauth2(i)
-				ad := &sql.OAuth2RequestSQL{}
-				for _, table := range tables {
-					require.NoError(t, d.Persister().Connection(context.Background()).RawQuery(fmt.Sprintf("select * from %s where signature = ?", table), ed.ID).First(ad))
-					assertEqualOauth2Data(t, ed, ad)
-				}
-
-				if i >= 11 {
-					abjti := &oauth2.BlacklistedJTI{}
-					require.NoError(t, c.Where("signature = ?", ebjti.ID).First(abjti))
-					assertEqualOauth2BlacklistedJTIs(t, ebjti, abjti)
-				}
-			}
-
-			x.CleanSQLPop(t, c)
-			require.NoError(t, c.Close())
+	if !testing.Short() {
+		dockertest.Parallel([]func(){
+			func() {
+				connections["postgres"] = dockertest.ConnectToTestPostgreSQLPop(t)
+			},
+			func() {
+				connections["mysql"] = dockertest.ConnectToTestMySQLPop(t)
+			},
+			func() {
+				connections["cockroach"] = dockertest.ConnectToTestCockroachDBPop(t)
+			},
 		})
 	}
+
+	var test = func(db string, c *pop.Connection) func(t *testing.T) {
+		return func(t *testing.T) {
+			defer func() {
+				if err := recover(); err != nil {
+					t.Fatalf("recovered: %+v\n\t%s", err, debug.Stack())
+				}
+			}()
+
+			t.Logf("Cleaning up before migrations")
+			_ = os.Remove("../migrations/sql/schema.sql")
+			testhelpers.CleanSQL(t, c)
+
+			t.Cleanup(func() {
+				t.Logf("Cleaning up after migrations")
+				testhelpers.CleanSQL(t, c)
+				require.NoError(t, c.Close())
+			})
+
+			url := c.URL()
+			// workaround for https://github.com/gobuffalo/pop/issues/538
+			switch db {
+			case "mysql":
+				url = "mysql://" + url
+			case "sqlite":
+				url = "sqlite3://" + url
+			}
+			t.Logf("URL: %s", url)
+
+			tm := popx.NewTestMigrator(t, c, "../migrations/sql", "./testdata")
+			require.NoError(t, tm.Up())
+
+			viper.Set(configuration.ViperKeyURLsSelfPublic, "https://www.ory.sh/")
+			viper.Set(configuration.ViperKeyDefaultIdentityTraitsSchemaURL, "file://stub/default.schema.json")
+			viper.Set(configuration.ViperKeyDSN, url)
+
+			d, err := driver.NewDefaultDriver(l, "", "", "", true)
+			require.NoError(t, err)
+
+			t.Run("suite=fixtures", func(t *testing.T) {
+				t.Run("case=identity", func(t *testing.T) {
+					ids, err := d.Registry().PrivilegedIdentityPool().ListIdentities(context.Background(), 100, 0)
+					require.NoError(t, err)
+
+					for _, id := range ids {
+						actual, err := d.Registry().PrivilegedIdentityPool().GetIdentityConfidential(context.Background(), id.ID)
+						require.NoError(t, err)
+						compareWithFixture(t, actual, "identity", id.ID.String())
+					}
+				})
+
+				t.Run("case=login", func(t *testing.T) {
+					var ids []login.Request
+					require.NoError(t, c.Select("id").All(&ids))
+
+					for _, id := range ids {
+						actual, err := d.Registry().LoginRequestPersister().GetLoginRequest(context.Background(), id.ID)
+						require.NoError(t, err)
+						compareWithFixture(t, actual, "login_request", id.ID.String())
+					}
+				})
+				t.Run("case=registration", func(t *testing.T) {
+					var ids []registration.Request
+					require.NoError(t, c.Select("id").All(&ids))
+
+					for _, id := range ids {
+						actual, err := d.Registry().RegistrationRequestPersister().GetRegistrationRequest(context.Background(), id.ID)
+						require.NoError(t, err)
+						compareWithFixture(t, actual, "registration_request", id.ID.String())
+					}
+				})
+				t.Run("case=settings_request", func(t *testing.T) {
+					var ids []settings.Request
+					require.NoError(t, c.Select("id").All(&ids))
+
+					for _, id := range ids {
+						actual, err := d.Registry().SettingsRequestPersister().GetSettingsRequest(context.Background(), id.ID)
+						require.NoError(t, err)
+						compareWithFixture(t, actual, "settings_request", id.ID.String())
+					}
+				})
+
+				t.Run("case=recovery_request", func(t *testing.T) {
+					var ids []recovery.Request
+					require.NoError(t, c.Select("id").All(&ids))
+
+					for _, id := range ids {
+						actual, err := d.Registry().RecoveryRequestPersister().GetRecoveryRequest(context.Background(), id.ID)
+						require.NoError(t, err)
+						compareWithFixture(t, actual, "recovery_request", id.ID.String())
+					}
+				})
+			})
+
+			t.Run("suite=constraints", func(t *testing.T) {
+				sr, err := d.Registry().SettingsRequestPersister().GetSettingsRequest(context.Background(), x.ParseUUID("a79bfcf1-68ae-49de-8b23-4f96921b8341"))
+				require.NoError(t, err)
+
+				require.NoError(t, d.Registry().PrivilegedIdentityPool().DeleteIdentity(context.Background(), sr.IdentityID))
+
+				_, err = d.Registry().SettingsRequestPersister().GetSettingsRequest(context.Background(), x.ParseUUID("a79bfcf1-68ae-49de-8b23-4f96921b8341"))
+				require.Error(t, err)
+				require.True(t, errors.Is(err, sqlcon.ErrNoRows))
+			})
+
+			require.NoError(t, tm.Down(-1))
+		}
+	}
+
+	for db, c := range connections {
+		t.Run(fmt.Sprintf("database=%s", db), test(db, c))
+	}
+}
+
+func compareWithFixture(t *testing.T, actual interface{}, prefix string, id string) {
+	location := filepath.Join("fixtures", prefix, id+".json")
+	expected, err := ioutil.ReadFile(location)
+	writeFixtureOnError(t, err, actual, location)
+
+	actualJSON, err := json.Marshal(actual)
+	require.NoError(t, err)
+
+	if !assert.JSONEq(t, string(expected), string(actualJSON)) {
+		writeFixtureOnError(t, nil, actual, location)
+	}
+}
+
+func writeFixtureOnError(t *testing.T, err error, actual interface{}, location string) {
+	if ok, _ := strconv.ParseBool(os.Getenv("REFRESH_FIXTURES")); ok {
+		content, err := json.MarshalIndent(actual, "", "  ")
+		require.NoError(t, err)
+		require.NoError(t, ioutil.WriteFile(location, content, 0666))
+	}
+	require.NoError(t, err)
 }
