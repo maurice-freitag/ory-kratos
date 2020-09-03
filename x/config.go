7ab3d2acf36d15f4f9ffcbf6aea468369cfca08e
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-09-03 15:21:29 +0200

    fix: resolve pkger issues

--- x/config.go
+++ x/config.go
@@ -2,22 +2,12 @@ package x
 
 import (
 	"github.com/markbates/pkger"
-
 	"github.com/ory/x/logrusx"
 	"github.com/ory/x/viperx"
 )
 
-var schema = MustPkgerRead(pkger.Open("/.schema/config.schema.json"))
-
 func WatchAndValidateViper(log *logrusx.Logger) {
-	file, err := pkger.Open("/.schema/config.schema.json")
-	if err != nil {
-		log.WithError(err).Fatal("Unable to open configuration JSON Schema.")
-	}
-	defer file.Close()
-	schema, err := ioutil.ReadAll(file)
-	if err != nil {
-		log.WithError(err).Fatal("Unable to read configuration JSON Schema.")
-	}
-	viperx.WatchAndValidateViper(log, schema, "ORY Kratos", []string{"serve", "profiling", "log"}, "")
+	schema := MustPkgerRead(pkger.Open("/.schema/config.schema.json"))
+	viperx.WatchAndValidateViper(log, schema,
+		"ORY Kratos", []string{"serve", "profiling", "log"}, "")
 }
