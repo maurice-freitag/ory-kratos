375311d0c84d698900cd25a392c36aa01be71ae3
zepatrik <patrik@ory.sh>
2020-09-24 17:50:15 +0200

    docs: improve long messages and render cli documentation

--- cmd/clidoc/main.go
+++ cmd/clidoc/main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"fmt"
+	"github.com/ory/kratos/cmd/remote"
+	"github.com/spf13/cobra"
 	"os"
 
 	"github.com/ory/x/clidoc"
 
-	"github.com/ory/hydra/cmd"
+	"github.com/ory/kratos/cmd/identities"
+	"github.com/ory/kratos/cmd/jsonnet"
 )
 
 func main() {
-	if err := clidoc.Generate(cmd.NewRootCmd(), os.Args[1:]); err != nil {
+	rootCmd := &cobra.Command{ Use: "kratos" }
+	identities.RegisterCommandRecursive(rootCmd)
+	jsonnet.RegisterCommandRecursive(rootCmd)
+	remote.RegisterCommandRecursive(rootCmd)
+
+	if err := clidoc.Generate(rootCmd, os.Args[1:]); err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "%+v", err)
 		os.Exit(1)
 	}
